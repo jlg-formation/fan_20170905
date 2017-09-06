65b3c4a00de6d64139dd6375eca07bd76a63e5c8
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-09-06 15:23:36 +0200

    changement note souris

--- app/cpv-star/cpv-star.js
+++ app/cpv-star/cpv-star.js
@@ -9,18 +9,25 @@
 			scope: {
 				n: '=?note'
 			},
-			controller: function CpvStarCtrl($scope, $element, $attrs, $transclude) {
+			controller: function CpvStarCtrl($scope, $element, $compile) {
 				// Surtout pas $element !
 				console.log('CpvStarCtrl', arguments);
-				let html = '';
-				let note = $scope.n || 3;
-				for (let i = 0; i < note; i++) {
-					html += '<img src="cpv-star/img/yellow_star.png">';
-				}
-				for (let i = note; i < 5; i++) {
-					html += '<img src="cpv-star/img/white_star.png">';
-				}
-				$element.html(html);
+				$scope.update = function(newNote) {
+					console.log('update', arguments);
+					$scope.n = newNote;
+				};
+				$scope.$watch('n', function() {
+					let html = '';
+					let note = $scope.n || 3;
+					for (let i = 0; i < note; i++) {
+						html += '<img ng-click="update(' + (i + 1) + ')" src="cpv-star/img/yellow_star.png">';
+					}
+					for (let i = note; i < 5; i++) {
+						html += `<img ng-click="update(${i+1})" src="cpv-star/img/white_star.png">`;
+					}
+					$element.html(html);
+					$compile($element.contents())($scope);
+				});
 			}
 		};
 	});
