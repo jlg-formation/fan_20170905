f85e526b71035e4375214a5e3508552ed087efeb
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-09-06 15:05:42 +0200

    version avec scope local

--- app/cpv-star/cpv-star.js
+++ app/cpv-star/cpv-star.js
@@ -6,15 +6,18 @@
 	app.directive('cpvStar', function() {
 		return {
 			restrict: 'E',
+			scope: {
+				n: '=?note'
+			},
 			controller: function CpvStarCtrl($scope, $element, $attrs, $transclude) {
-                // Surtout pas $element !
+				// Surtout pas $element !
 				console.log('CpvStarCtrl', arguments);
-                let html = '';
-                let note = $attrs.note || 3;
+				let html = '';
+				let note = $scope.n || 3;
 				for (let i = 0; i < note; i++) {
 					html += '<img src="cpv-star/img/yellow_star.png">';
-                }
-                for (let i = note; i < 5; i++) {
+				}
+				for (let i = note; i < 5; i++) {
 					html += '<img src="cpv-star/img/white_star.png">';
 				}
 				$element.html(html);
