47d46c9abf1e70bc1f61af98ac049be02eb27656
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-09-06 13:31:07 +0200

    5 stars

--- app/cpv-star/cpv-star.js
+++ app/cpv-star/cpv-star.js
@@ -3,14 +3,22 @@
 
 	const app = angular.module('cpv-star', []);
 
-    app.directive('cpvStar', function () {
+	app.directive('cpvStar', function() {
 		return {
-            restrict: 'E',
-            controller: function CpvStarCtrl($scope, $element, $attrs, $transclude) {
-                console.log('CpvStarCtrl', arguments);
-                $element.html('<b>Mr. Sarr</b>');
-            }
+			restrict: 'E',
+			controller: function CpvStarCtrl($scope, $element, $attrs, $transclude) {
+				console.log('CpvStarCtrl', arguments);
+                let html = '';
+                let note = 3;
+				for (let i = 0; i < note; i++) {
+					html += '<img src="cpv-star/img/yellow_star.png">';
+                }
+                for (let i = note; i < 5; i++) {
+					html += '<img src="cpv-star/img/white_star.png">';
+				}
+				$element.html(html);
+			}
 		};
-    });
-    
-  })();
+	});
+
+})();
